f03b553a40d861ba02999703f8e2851cf61e6b6f
Suraj Kumar <ravi.suraj110@gmail.com>
2022-08-20 17:21:41 +0530

    create a mix todo project

---
+++ todo/lib/todo/list.ex
@@ -0,0 +1,41 @@
+defmodule Todo.List do
+  use GenServer
+
+  # Callback functions executed on server interface
+
+  def init(_), do: {:ok, []}
+
+  def handle_call({:get, date_tuple}, _, state) do
+    {:reply, Enum.filter(state, &(&1.date == date_tuple)), state}
+  end
+
+  def handle_call(:list, _, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:put, todo}, state) do
+    {:noreply, [todo | state]}
+  end
+
+  def handle_info(_, state) do
+    {:noreply, state}
+  end
+
+  # Generic code called by the client
+
+  def start do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def add(pid, todo) do
+    GenServer.cast(pid, {:put, todo})
+  end
+
+  def filter(pid, date) do
+    GenServer.call(pid, {:get, date})
+  end
+
+  def list(pid) do
+    GenServer.call(pid, :list)
+  end
+end
